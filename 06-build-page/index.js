2aac2286021d3d30151e3930574287ce7ccbc546
Zhansaule Telisheva <jansaule97@mail.ru>
2024-01-22 20:32:47 +0500

    feat: combine all styles into one style.css file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const writableStream = fs.createWriteStream(__dirname + '/project-dist/style.css');
 
 // create a project-dist folder
 fs.mkdir(path.join(__dirname, 'project-dist'),
@@ -20,7 +21,7 @@ fs.mkdir(path.join(__dirname + '/project-dist', 'assets'),
 const src = __dirname + '/assets';
 const dest = __dirname + '/project-dist/assets';
 
-// copy function 
+// copy function to copy assets folder to /project-dist/assets folder
 const copy = (src, dest) => {
   fs.readdir(src, (err, files) => {
   if (err) {
@@ -58,3 +59,33 @@ const copy = (src, dest) => {
 };
 
 copy(src, dest);
+
+
+// combine all styles to style.css file
+fs.readdir(__dirname + '/styles', (err, files) => {
+
+  if (err) {
+    throw err;
+  }
+
+  files.forEach(file => {
+    fs.stat(__dirname + '/styles/' + file, (err, fileStats) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (fileStats.isFile() && path.extname(file).split(".")[1] == 'css') {
+          fs.readFile(__dirname + '/styles/' + file, function (err, data) {
+            if (err) throw err;
+
+            fs.appendFile(__dirname + '/project-dist/style.css', data, function (err) {
+              if (err) throw err;
+            }
+            );
+          });
+
+        }
+      }
+    });
+  });
+
+});
